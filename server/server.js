bccccff7e0f5880bdeb10fa86f56664f3715c124
Riccardo Sangiorgio <richard.sangiorgio@gmail.com>
2018-06-29 09:09:53 +0000

    Commentato Helmet.

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http')
 const bodyParser = require('body-parser')
 const socketIO = require('socket.io')
 const path = require('path')
-const helmet = require('helmet')
+// const helmet = require('helmet')
 const history = require('connect-history-api-fallback')
 
 var app = express()
@@ -15,7 +15,7 @@ const port = process.env.PORT || 3000
 app.use(history())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(helmet())
+// app.use(helmet())
 
 app.use(express.static(path.join(__dirname, '/public')))
 
