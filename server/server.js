894c11a1c7bba71dc6df8ea22402e90bfeb7cf94
Riccardo Sangiorgio <richard.sangiorgio@gmail.com>
2018-06-29 09:16:30 +0000

    Rimosso Helmet e aggiornate le dipendenze.

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const http = require('http')
 const bodyParser = require('body-parser')
 const socketIO = require('socket.io')
 const path = require('path')
-// const helmet = require('helmet')
 const history = require('connect-history-api-fallback')
 
 var app = express()
@@ -15,7 +14,6 @@ const port = process.env.PORT || 3000
 app.use(history())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-// app.use(helmet())
 
 app.use(express.static(path.join(__dirname, '/public')))
 
